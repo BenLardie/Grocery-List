0fda0857f616b1aaa9e91c2954a4d91a4cd60fa2
Ben Lardie <benlardie@gmail.com>
2019-04-07 18:06:00 -0400

    some progress but form still not submitting

--- src/Form.js
+++ src/Form.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
 
 const Form = () => {
+
+const itemRef = useRef();
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const newItem = {
+    name: itemRef.current.value,
+    type: 'prod',
+    quantity: 1 
+  }; 
+
+  onSubmit(newItem);
+
+};
+
+
 return (
-  <form id="newItem" className="newitem" auto-complete="off">
-    <label htmlFor="itemName" className="line-label">New Item</label>
+  <form onSubmit={handleSubmit} id="newItem" className="newitem" auto-complete="off">
+    <label htmlFor="itemName" className="line-label">New Item </label>
     <div className="addnew">
-      <input type="text" name="item" id="itemName" className="form-component inpt" placeholder="What do you need?" />
+      <input ref={itemRef} type="text" name="item" id="itemName" className="form-component inpt" placeholder="What do you need?" />
       <input type="submit" value="Add" className="form-component btn" />
     </div>
   </form>
